795568d4c977dc34b0123ff6922f042830a6e086
Yagua <yagua.db@gmail.com>
2022-01-04 19:39:51 -0500

    fix bug in set_options function

--- lua/nebulous/config.lua
+++ lua/nebulous/config.lua
@@ -17,14 +17,15 @@ M.scheme_options = {
   custom_colors = {},
 }
 
+local _user_config = {}
+
 ---Set custom options to the editor
 --@parama opts table: custom options for editor
 function M.set_options(opts)
   opts = opts or {}
-  if type(opts) == "table" and next(opts) ~= nil then
-    return vim.tbl_deep_extend("force", {}, M.scheme_options, opts)
-  end
-  return M.scheme_options
+  local options = vim.tbl_deep_extend("force", {}, M.scheme_options, opts)
+  _user_config = options
+  return options
 end
 
 ---Get the value of secheme options
@@ -32,15 +33,14 @@ end
 --@returns settings table: settings adapted to load the scheme
 function M.get_options()
   local settings = {} --TODO: improve options adaptation
-  local opts = M.scheme_options
 
-  settings.st_eof = opts.disable.endOfBuffer
-  settings.term_colors = opts.disable.terminal_colors
-  settings.st_disable_bg = opts.disable.background and "NONE"
-  settings.st_comments = opts.italic.comments and "italic" or "NONE"
-  settings.st_keywords = opts.italic.keywords and "italic" or "NONE"
-  settings.st_functions = opts.italic.functions and "italic" or "NONE"
-  settings.st_variables = opts.italic.variables and "italic" or "NONE"
+  settings.st_eof = _user_config.disable.endOfBuffer
+  settings.term_colors = _user_config.disable.terminal_colors
+  settings.st_disable_bg = _user_config.disable.background and "NONE"
+  settings.st_comments = _user_config.italic.comments and "italic" or "NONE"
+  settings.st_keywords = _user_config.italic.keywords and "italic" or "NONE"
+  settings.st_functions = _user_config.italic.functions and "italic" or "NONE"
+  settings.st_variables = _user_config.italic.variables and "italic" or "NONE"
 
   return settings
 end
